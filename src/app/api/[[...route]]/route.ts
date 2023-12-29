49a2387ac0bad114521e9fb7025144b5f128f0d8
waynemamahit <wayne.mamahit@gmail.com>
2023-12-29 21:30:02 +0700

    Add rate limiter

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,3 +1,4 @@
+import { limiter } from '@/server/middlewares/limiter.middleware';
 import authRoute from '@/server/routes/auth.route';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
@@ -5,15 +6,21 @@ import { logger } from 'hono/logger';
 import { poweredBy } from 'hono/powered-by';
 import { secureHeaders } from 'hono/secure-headers';
 import { handle } from 'hono/vercel';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api/v1');
+const rateLimiter = new RateLimiterMemory({
+  points: 20,
+  duration: 10,
+});
 
 app.use('*', cors());
 app.use('*', logger());
 app.use('*', poweredBy());
 app.use('*', secureHeaders());
+app.use('*', limiter(rateLimiter));
 
 app.route('/auth', authRoute);
 
---
+++ src/server/middlewares/limiter.middleware.ts
@@ -0,0 +1,14 @@
+import { Context, Next } from "hono";
+import { RateLimiterMemory } from "rate-limiter-flexible";
+
+export const limiter = (memory: RateLimiterMemory) => async (c: Context, next: Next) => {
+  try {
+    await memory.consume(c.req.url, 1);
+    await next()
+  } catch {
+    return c.json({
+      message: 'To many requests!',
+      statuscode: 429,
+    }, 429)
+  }
+}

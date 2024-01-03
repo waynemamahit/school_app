c769aa4adab7f24446bf0734e520a80bfa981b9d
waynemamahit <wayne.mamahit@gmail.com>
2024-01-03 16:18:26 +0700

    Update schema and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
-import { Card, CardActions, CardContent, Grid, Typography } from '@mui/material'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
 
 export default function Home() {
   return (
     <main>
-      <Grid container justifyContent={'center'} alignItems={'center'} height={'80vh'}>
+      <Grid
+        container
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'80vh'}
+      >
         <Grid item xs={12} sm={6} md={6} lg={4}>
-          <Card variant='outlined'>
+          <Card variant="outlined">
             <CardContent>
-              <Typography variant='h4' textAlign={'center'}>Hello World!</Typography>
+              <Typography
+                variant="h4"
+                textAlign={'center'}
+                className="text-blue-800"
+              >
+                Hello World!
+              </Typography>
             </CardContent>
-            <CardActions>
-            </CardActions>
+            <CardActions></CardActions>
           </Card>
         </Grid>
       </Grid>
     </main>
-  )
+  );
 }
